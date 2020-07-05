63c90cc3232baf094d554f0a1dd332fd73402e05
gametest <1801028@s.asojuku.ac.jp>
2020-07-05 11:01:38 +0000

    src

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <hello-world v-bind:title="message"
+
+    v-on:result-event="doAction"
+    v-on:table="doAction"/>
+    <p>{{ result }}</p>
+    
+    
+    
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/Mylist.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+    
+  },
+  
+  methods:{
+    doAction2:function(){
+      //ここにデータを入れる
+      
+    },
+    /*appAction:function(){
+      this.result = '(*** you send:"'+ message +' ***)';
+      
+      //ここにデータを入れる
+        var data = {
+    items:[
+      {name:'at'}
+      ]
+      }
+    }*/
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Mylist.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="hello">
+    <h1>{{ title }}</h1>
+    
+  <input type ="text" v-model="input">
+  <button v-on:click="doAction">click</button>
+  <hr>
+  <p v-html="list"></p>
+  <p><table><tr><th>mylist</th></tr>
+     <tr><td>{{ list }}</td></tr>
+    </table></p>
+
+  <p>{{ list }}</p>
+  <copted v-model="msg"></copted>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    title: String,
+    //message: String,
+    list:String,
+    
+  },
+  data:function(){
+    return {
+      message:'what your name?',
+      input:'mylist',
+    };
+  },
+  //ここにmylistで入れ
+  methods:{
+    doAction:function(){
+      /*var list{
+        
+      }*/
+      //this.list = this.input + 'さん';
+      this.list = this.input + document.createElement('table');
+      var row = table.insertRow( -1 );
+      //ここを書き換える？
+      this.$emit('table',this.input);
+      this.$emit('result-event',this.input);
+    }
+   
+  }
+}
+</script>
+
+<style>
+div{
+  margin:0px;
+  padding: 0px;
+  text-align: center;
+}
+h1{
+  font-size: 20pt;
+  font-weight: bold;
+  text-align: center;
+  color: black;
+  margin: 0px;
+}
+
+table tr th{
+  padding: 0px;
+  text-align: center;
+  color:white;
+  background-color: black;
+  margin: 0px;
+}
+table tr td{
+  padding: 0px;
+  text-align: center;
+  border:1px solid gray;
+  margin: 0px;
+}
+
+</style>
